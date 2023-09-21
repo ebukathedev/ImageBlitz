2f36478c75dc5cdaa6e5c49d47f02b666698257b
ebukathedev <ebuskelele@gmail.com>
2023-09-21 09:20:16 +0100

    abstract personal data

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyCidUovFAqtwUMdvl5_tJ64QASNFvoI9PM",
-	authDomain: "imageblitz.firebaseapp.com",
-	projectId: "imageblitz",
-	storageBucket: "imageblitz.appspot.com",
-	messagingSenderId: "114550197027",
-	appId: "1:114550197027:web:79b263c16e8aa37b461a96",
-	measurementId: "G-TKGN20KQJW",
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_APP_ID,
+	measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
